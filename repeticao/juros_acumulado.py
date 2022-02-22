a724e8521493bdf01fbdc0e635d85d58e1e64816
Clecio Sousa <clecio1201@hotmail.com>
2022-02-22 18:40:19 -0300

    initial commit

--- repeticao/juros_acumulado.py
+++ repeticao/juros_acumulado.py
@@ -1,5 +1,12 @@
 deposito_inicial = float(input("Digite o valor do deposito inicial: "))
 tx_juros = float(input("Taxa de juros: "))
 qtde_parcelas = int(input("Digite a quantidade de parcelas: "))
+parcela = 1
+novo_valor = deposito_inicial
 
-while qtde_parcelas > 0:
+while parcela <= qtde_parcelas:
+    novo_valor = novo_valor + (novo_valor * (tx_juros / 100))
+    print(f"{parcela}ª Parcela: R${novo_valor:8.2f}")
+    parcela += 1
+
+print(f"novo valor: R${novo_valor:8.2f}")
