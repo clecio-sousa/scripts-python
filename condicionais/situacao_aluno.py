cf583392e6669bd5bc0afd726b0be8439598a923
cleciosousa <clecio1201@hotmail.com>
2022-02-24 16:36:51 -0300

    commit initial

--- condicionais/situacao_aluno.py
+++ condicionais/situacao_aluno.py
@@ -1,33 +1,24 @@
 nome = input("Digite o nome do aluno: ")
 idade = int(input("Digite a idade do aluno: "))
 serie = input("Digite a serie do aluno: ")
-soma_notas = 1
-qtde_notas = 1
-media = soma_notas/qtde_notas
+
 
 #  Condicoes de aprovacao
-aprovado = media >= 7
-trabalho_extra = 7 >= media >= 5
-recuperacao = 5 >= media >= 4
-reprovado = media < 4
-################################
+n1 = float(input("digite a nota: "))
+n2 = float(input("digite a nota: "))
+n3 = float(input("digite a nota: "))
+n4 = float(input("digite a nota: "))
 
-while True:
-    if aprovado:
-        print(f"Aluno aprovado. Media: {media}")
+media = (n1 + n2 + n3 + n4)/4
 
-    elif trabalho_extra:
-        print(f"Aluno aprovado. Media: {media}")
+if media >= 7:
+    print("aprovado")
+elif 5 <= media <= 7:
+    print("trabalho extra")
 
-    elif recuperacao:
-        print(f"Aluno aprovado. Media: {media}")
+elif 5 >= media <= 4:
+    print("recuperacao")
 
-    elif reprovado:
-        print(f"Aluno aprovado. Media: {media}")
-
-    notas = float(input("Digite a %dª nota do aluno (0 para sair):" % qtde_notas))
-    qtde_notas += 1
-    soma_notas = soma_notas + qtde_notas
-
-
+elif media < 4:
+    print("reprovado")
 
