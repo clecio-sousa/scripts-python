f4802a644eef6bef9220a62b998bf7a1cb8aaeb7
Clecio Sousa <clecio1201@hotmail.com>
2022-02-24 19:06:05 -0300

    initial commit

--- condicionais/situacao_aluno.py
+++ condicionais/situacao_aluno.py
@@ -2,8 +2,38 @@ nome = input("Digite o nome do aluno: ")
 idade = int(input("Digite a idade do aluno: "))
 serie = input("Digite a serie do aluno: ")
 
+qtde_notas = 0
+soma_notas = 0
 
-#  Condicoes de aprovacao
+while True:
+    nota = float(input("Digite a nota: "))
+    if nota == 0:
+        break
+    else:
+        soma_notas = soma_notas + nota
+        qtde_notas = qtde_notas + 1
+
+media = float(soma_notas / qtde_notas)
+aprovado = media >= 7
+trabalho_extra = 5 <= media <= 7
+recuperacao = 4 <= media <= 5
+reprovado = media <= 4
+
+if aprovado:
+    print("APROVADO. PARABEnS. Media:{:.2f} ".format(media))
+elif trabalho_extra:
+    print("FAZER TRABALHO EXTRA. Media:{:.2f} ".format(media))
+
+elif recuperacao:
+    print("RECUPERAÇÃO. Media:{:.2f} ".format(media))
+
+elif reprovado:
+    print("REPROVADO. Media:{:.2f} ".format(media))
+
+
+
+
+"""#  Condicoes de aprovacao
 n1 = float(input("digite a nota: "))
 n2 = float(input("digite a nota: "))
 n3 = float(input("digite a nota: "))
@@ -21,4 +51,4 @@ elif 5 >= media <= 4:
 
 elif media < 4:
     print("reprovado")
-
+"""
