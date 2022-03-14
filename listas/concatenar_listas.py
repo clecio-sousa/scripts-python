4fd25e457804d680361d6f5babc007fe3c0b1600
cleciosousa <clecio1201@hotmail.com>
2022-03-14 07:23:28 -0300

    commit initial

--- listas/concatenar_listas.py
+++ listas/concatenar_listas.py
@@ -3,10 +3,10 @@ lista_b = []
 lista_inter = []
 
 for i in range(10):
-    lista_a.append(int(input("Digite o %dº numero: " % (i + 1) + '\n')))
+    lista_a.append(int(input("Digite o %dº numero: " % (i + 1))))
 
 for j in range(10):
-    lista_b.append(int(input("Digite o %dº numero: " % (j + 1) + '\n')))
+    lista_b.append(int(input("Digite o %dº numero: " % (j + 1))))
 
 for k in range(10):
     lista_inter.append(lista_a[k])
