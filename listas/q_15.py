f76ff25580680bf65daca9d995c8d2e4a2b841f4
cleciosousa <clecio1201@hotmail.com>
2022-03-15 16:17:35 -0300

    commit initial

--- listas/q_15.py
+++ listas/q_15.py
@@ -1,16 +1,31 @@
 lista = []
+lista_acima_media = []
 numero = 1
+soma = 0
 
 while True:
-    i = int(input(" Digite o %dº número: " % numero))
+    i = float(input(" Digite o %dº número: " % numero))
     if i == -1:
         break
-    lista.append(i)
+    soma = soma + i
     numero += 1
+    lista.append(i)
+
 
 print("Quantidade de valores lidos: {}" .format(len(lista)))
 for j in lista:
-    print(j, end='-', '\n')
+    print(j, end='-')
 
+print("\n")
 for z in lista:
-    print(z)
+    lista.sort(reverse=True)
+media = soma/numero
+
+for m in lista:
+    if m > media:
+        lista_acima_media.append(m)
+
+print(lista)
+print("Soma dos valores : {}".format(soma))
+print("Media dos valores : {}".format(media))
+print("Notas acima da média: {}".format(lista_acima_media))
