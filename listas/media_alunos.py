27979deeaa3cd7c923df9a0566a4b77ae2af4085
Clecio Sousa <clecio1201@hotmail.com>
2022-03-16 00:07:47 -0300

    initial commit

--- listas/media_alunos.py
+++ listas/media_alunos.py
@@ -2,12 +2,28 @@ notas = []
 alunos = []
 qtde_nota = 0
 soma = 0
+media = 0
+aprovados = []
 
-for i in range(2):
-    alunos.append(input("Nome do aluno: " + str(i + 1)))
+for i in range(1):
+    alunos.append(input("Nome do aluno: "))
+
     for j in range(4):
-
         notas.append(float(input("Digite a %dª nota: " % (j + 1))))
-        soma += j
+        soma = soma + notas[j]
         qtde_nota += 1
-        media = soma/qtde_nota
+        media = media + notas[j]
+
+media = float(soma/qtde_nota)
+if media >= 7.0:
+    print("Aluno: {}".format(alunos))
+    print(media)
+else:
+    print(media)
+
+
+
+
+
+
+
