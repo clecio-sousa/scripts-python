7b25079f0463a34e77e959813166c4c5ee84275e
Clecio Sousa <clecio1201@hotmail.com>
2023-07-11 01:05:07 -0300

    commit

--- condicionais/jogo_adivinhacao.py
+++ condicionais/jogo_adivinhacao.py
@@ -3,6 +3,16 @@ print("*  JOGO DA ADIVINHAÇÃO  *")
 print("*************************")
 
 numero_secreto = 42
+numero = 75
+
+segundo_numero = 32
+triplo = 4
+terceiro = 89
+nome = 'clecio'
+sobre_nome = 'clecio'
+
+
+
 
 chute = int(input("Digite seu número:"))
 print("Você digitou {}".format(chute))
@@ -11,11 +21,11 @@ acertou = chute == numero_secreto
 maior = chute > numero_secreto
 menor = chute < numero_secreto
 
-if(acertou):
+if acertou:
     print("Vc acertou. Numero secreto: {} ".format(numero_secreto))
 
-elif(maior):
+elif maior:
     print("Vc errou!!! O numero secreto é menor!!!")
 
-elif(menor):
+elif menor:
     print("Vc errou!!! O Numero secreto é maior!!!")
