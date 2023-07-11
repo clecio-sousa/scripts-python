07d52bf8f0c48784d308d0892eea75b3fa344812
Clecio Sousa <clecio1201@hotmail.com>
2023-07-11 01:04:51 -0300

    multa por velocidade

--- condicionais/multa_velocidade.py
+++ condicionais/multa_velocidade.py
@@ -3,9 +3,14 @@ velocidade = float(input("Digite a velocidade do carro:"))
 velocidade_permitida = 80
 
 velocidade_maior = velocidade > 80
+valor_multa = (velocidade - velocidade_permitida) * 5
 
-if(velocidade_maior):
-    print("Carro multado. Sua velocidade {} .".format(velocidade))
+if velocidade_maior:
+    print(f'Você levou multa por exceder velocidade. \n'
+          f'Velocidade registrada: {velocidade}\n'
+          f'Velocidade permitida: {velocidade_permitida}\n'
+          f'---------------------------\n'
+          f'VALOR TOTAL DA MULTA:{valor_multa}')
 
 else:
-    print("O carro não foi multado")
+    print(" Você não foi multado.")
