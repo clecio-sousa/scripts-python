9b7f1ee03c1fb9e32ceb3511a7b5ab4a67da8a59
Clecio Sousa <clecio1201@hotmail.com>
2023-07-18 22:02:17 -0300

    commit

--- condicionais/emprestimo_bancario.py
+++ condicionais/emprestimo_bancario.py
@@ -5,12 +5,10 @@ qtde_anos_pagar = int(input("Quantidade de anos a pagar:"))
 prestacao = valor_casa / (qtde_anos_pagar * 12)
 if prestacao > salario * 0.3:
 
-
-
     print("EMPRESTIMO NÃO APROVADO\n"
-            f"SALARIO: {salario}\n"
-            f"PRESTAÇÃO: {prestacao:.2f}")
+          f"SALARIO: {salario}\n"
+          f"PRESTAÇÃO: {prestacao:.2f}")
 else:
 
-     print('-------------------------\n'
-            f'VALOR DA PRESTAÇÃO: {prestacao:.2f}')
+    print('-------------------------\n'
+          f'VALOR DA PRESTAÇÃO: {prestacao:.2f}')
